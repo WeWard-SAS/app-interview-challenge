3cdc5be4aeb04fa9904b0c532963f5563e28a10e
AIT ABDELMALEK Abdellah <abdellahaa@bam.tech>
2022-12-21 23:35:25 +0100

    feat: add months navigation

--- src/CalendarHeader.tsx
+++ src/CalendarHeader.tsx
@@ -15,16 +15,45 @@ export const CalendarHeader = ({
 }: CalendarHeaderProps) => {
   const strMonth = monthNames[month - 1];
 
+  const onNavToNextMonth = () => {
+    if (month === 12) {
+      setMonth(1);
+      setYear(year + 1);
+    } else {
+      setMonth(month + 1);
+    }
+  };
+
+  const onNavToPreviousMonth = () => {
+    if (month === 1) {
+      setMonth(12);
+      setYear(year - 1);
+    } else {
+      setMonth(month - 1);
+    }
+  };
+
   return (
     <View>
       <View
         style={{
           borderBottomColor: 'black',
           borderBottomWidth: 1,
+          flexDirection: 'row',
+          justifyContent: 'space-between',
         }}>
         <Text style={{margin: 5, fontSize: 16}}>
           {strMonth} {year}
         </Text>
+
+        <View style={{flexDirection: 'row'}}>
+          <TouchableOpacity style={{margin: 5}} onPress={onNavToPreviousMonth}>
+            <Text>{'<'}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={{margin: 5}} onPress={onNavToNextMonth}>
+            <Text>{'>'}</Text>
+          </TouchableOpacity>
+        </View>
       </View>
 
       <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
